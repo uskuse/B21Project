d34ed21cff924be91076739ff12999bcc9756c3b
Mustafa <uskuse@gmail.com>
2021-01-31 19:37:03 +0100

    print fine

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         //mac users and windows users are happy
         //everybdoy are happy.
+        //we are going to do well
         System.out.print("Everybody are fine");
     }
 
