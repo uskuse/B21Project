12041d3590ab3b499208d232c20ca4bd3912efdf
Mustafa <uskuse@gmail.com>
2021-02-07 16:27:39 +0100

    coding is funn

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //write your code here
         System.out.println("Hello");
         System.out.println("Git helps always");
+        //eu4 is excited
     }
 }
